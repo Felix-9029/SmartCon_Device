c3abc7b76b1e59e7ceb301634041acb311a79afa
Felix-9029 <65834291+Felix-9029@users.noreply.github.com>
2022-06-30 12:22:19 +0200

    add header

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
+/**
+ * @author <p>Nils Herzig, Felix Reichert</p>
+ * <p>Package: de.fr_nh_hl.easyled.ui</p>
+ * <p>File: MainActivity.kt</p>
+ * <p>Creation date: 01.06.2022</p>
+ * <p>Last update: 30.06.2022</p>
+ * <p>Version: 1</p>
+ */
+
 #include <Adafruit_NeoPixel.h>
 #include <ArduinoJson.h>
 #include <string>
@@ -74,101 +83,10 @@ void connectToWifi() {
 
 // ------------------------------------- OTA - INSECURE -------------------------------------
 
-const char *loginIndex =
-        "<form name='loginForm'>"
-        "<table width='20%' bgcolor='A09F9F' align='center'>"
-        "<tr>"
-        "<td colspan=2>"
-        "<center><font size=4><b>ESP32 Login Page</b></font></center>"
-        "<br>"
-        "</td>"
-        "<br>"
-        "<br>"
-        "</tr>"
-        "<td>Username:</td>"
-        "<td><input type='text' size=25 name='userid'><br></td>"
-        "</tr>"
-        "<br>"
-        "<br>"
-        "<tr>"
-        "<td>Password:</td>"
-        "<td><input type='Password' size=25 name='pwd'><br></td>"
-        "<br>"
-        "<br>"
-        "</tr>"
-        "<tr>"
-        "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
-        "</tr>"
-        "</table>"
-        "</form>"
-        "<script>"
-        "function check(form)"
-        "{"
-        "if(form.userid.value=='admin' && form.pwd.value=='admin')"
-        "{"
-        "window.open('/serverIndex')"
-        "}"
-        "else"
-        "{"
-        " alert('Error Password or Username')/*displays error message*/"
-        "}"
-        "}"
-        "</script>";
-
-/*
- * Server Index Page
- */
-
-const char *serverIndex =
-        "<script "
-        "src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></"
-        "script>"
-        "<form method='POST' action='#' enctype='multipart/form-data' "
-        "id='upload_form'>"
-        "<input type='file' name='update'>"
-        "<input type='submit' value='Update'>"
-        "</form>"
-        "<div id='prg'>progress: 0%</div>"
-        "<script>"
-        "$('form').submit(function(e){"
-        "e.preventDefault();"
-        "var form = $('#upload_form')[0];"
-        "var data = new FormData(form);"
-        " $.ajax({"
-        "url: '/update',"
-        "type: 'POST',"
-        "data: data,"
-        "contentType: false,"
-        "processData:false,"
-        "xhr: function() {"
-        "var xhr = new window.XMLHttpRequest();"
-        "xhr.upload.addEventListener('progress', function(evt) {"
-        "if (evt.lengthComputable) {"
-        "var per = evt.loaded / evt.total;"
-        "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
-        "}"
-        "}, false);"
-        "return xhr;"
-        "},"
-        "success:function(d, s) {"
-        "console.log('success!')"
-        "},"
-        "error: function (a, b, c) {"
-        "}"
-        "});"
-        "});"
-        "</script>";
 
 void handleUpdate() {
-    server.on("/", HTTP_GET, []() {
-        server.sendHeader("Connection", "close");
-        server.send(200, "text/html", loginIndex);
-    });
-    server.on("/serverIndex", HTTP_GET, []() {
-        server.sendHeader("Connection", "close");
-        server.send(200, "text/html", serverIndex);
-    });
-    /*handling uploading firmware file */
+
+    // TODO handling uploading firmware file
     server.on(
             "/update", HTTP_POST, []() {
                 server.sendHeader("Connection", "close");
@@ -183,16 +101,19 @@ void handleUpdate() {
                             UPDATE_SIZE_UNKNOWN)) {  // start with max available size
                         Update.printError(Serial);
                     }
-                } else if (upload.status == UPLOAD_FILE_WRITE) {
+                }
+                else if (upload.status == UPLOAD_FILE_WRITE) {
                     /* flashing firmware to ESP*/
                     if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
                         Update.printError(Serial);
                     }
-                } else if (upload.status == UPLOAD_FILE_END) {
+                }
+                else if (upload.status == UPLOAD_FILE_END) {
                     if (Update.end(
                             true)) {  // true to set the size to the current progress
                         Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
-                    } else {
+                    }
+                    else {
                         Update.printError(Serial);
                     }
                 }
@@ -211,19 +132,18 @@ void colorSet(uint32_t color) {
 
 // ------------------------------------- strip set globalAnimation -------------------------------------
 
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
+// rainbow animation -> r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
     WheelPos = 255 - WheelPos;
-    if(WheelPos < 85) {
-        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    if (WheelPos < 85) {
+        return Adafruit_NeoPixel::Color(255 - WheelPos * 3, 0, WheelPos * 3);
     }
-    if(WheelPos < 170) {
+    if (WheelPos < 170) {
         WheelPos -= 85;
-        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+        return Adafruit_NeoPixel::Color(0, WheelPos * 3, 255 - WheelPos * 3);
     }
     WheelPos -= 170;
-    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+    return Adafruit_NeoPixel::Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
 
@@ -241,12 +161,13 @@ uint32_t Wheel(byte WheelPos) {
 
     uint16_t i, j;
 
-    while(true) {
+    while (true) {
         for (j = 0; j < 256; j++) {
             for (i = 0; i < strip.numPixels(); i++) {
                 strip.setPixelColor(i, Wheel((i + j) & 255));
             }
             strip.show();
+            // delay replacement
             while (true) {
                 currentMillis = millis();
                 if (currentMillis - previousMillis > interval) {
@@ -333,7 +254,8 @@ void handlePost() {
         Serial.printf("R: %d G: %d B: %d W: %d\n", globalRed, globalGreen, globalBlue, globalWhite);
         colorSet(Adafruit_NeoPixel::Color(lightApplyBrightness(globalGreen), lightApplyBrightness(globalRed), lightApplyBrightness(globalBlue), stateOn ? globalWhite : 0));
         strip.show();
-    } else {
+    }
+    else {
         globalAnimation = jsonDocument["globalAnimation"].as<std::string>();
         if (AnimationTask != nullptr) {
             vTaskDelete(AnimationTask);
