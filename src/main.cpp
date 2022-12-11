ca380f7dceff515b56723577e9d9a458795c8ae3
Felix-9029 <65834291+Felix-9029@users.noreply.github.com>
2022-12-11 14:32:53 +0100

    update dependencies; add check for valid pins

--- src/main.cpp
+++ src/main.cpp
@@ -173,6 +173,31 @@ void handlePost() {
         return;
     }
 
+    // exclude unusable pins
+    if (pin < 0 // does not exist
+            || pin == 0 // boot mode (BOOT button)
+            || pin == 1 // TX
+            || pin == 2 // internal LED
+            || pin == 3 // RX
+            || (pin >= 6 && pin <= 11) // flash pins
+
+            // || pin == 12 // MUST be low on startup AND blocked only in dev mode -> debugging port
+            // || pin == 13 // blocked only in dev mode -> debugging port
+            // || pin == 14 // blocked only in dev mode -> debugging port
+            // || pin == 15 // blocked only in dev mode -> debugging port
+
+            || pin == 20 // does not exist
+            || pin == 24 // does not exist
+            || (pin >= 28 && pin <= 31) // does not exist
+            || (pin >= 34 && pin <= 36) // input only
+            || pin == 37 // does not exist
+            || pin == 38 // does not exist
+            || pin == 39 // input only
+            || pin >= 40 /* does not exist */) {
+        server.send(404, "application/json", "{}");
+        return;
+    }
+
     boolean containsPin = false;
     LedStripeOnPin* ledStripeOnPin;
     LedStripeOnPin* ledStripeOnPinOld;
